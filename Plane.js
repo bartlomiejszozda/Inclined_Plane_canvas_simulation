7fbf44565a55ef8af57cdaa485d75d2d1a14e35a
Bartlomiej Szozda <bartekszozda19@gmail.com>
2017-12-29 02:16:36 +0100

    first

---
+++ Plane.js
@@ -0,0 +1,212 @@
+function Rectangle (height, width, positionX, positionY, velocityX, velocityY, accelerationX, accelerationY, frictionX, frictionY) 
+{
+	this.height = height;
+    this.width = width;
+	this.pX = positionX;
+    this.pY = positionY;
+	this.startpX=positionX;
+	this.startpY=positionY;
+	this.vX = velocityX;
+	this.vY = velocityY;
+	this.aX = accelerationX;
+	this.aY = accelerationY;
+	this.frictionX = frictionX;
+	this.frictionY = frictionY;
+	this.updateParameters=updateParameters;
+	this.nextPosition=nextPosition;
+	this.drawRectangle=drawRectangle;
+	this.time=0.0;
+	this.number=0;
+  }
+  
+  
+  
+  function updateParameters(positionX=this.pX, positionY=this.pY, velocityX=this.vX, velocityY=this.vY, accelerationX=this.aX, accelerationY=this.aY, height=this.height, width=this.width, frictionX=this.frictionX, frictionY=this.frictionY, time=this.time) 
+  {
+    this.height = height;
+    this.width = width;
+	this.pX = positionX;
+    this.pY = positionY;
+	this.vX = velocityX;
+	this.vY = velocityY;
+	this.aX = accelerationX;
+	this.aY = accelerationY;
+	this.frictionX = frictionX;
+	this.frictionY = frictionY;
+  }
+  function nextPosition(dt)
+  {
+	var frictionSign=null;
+this.number+=1;
+	  this.time=this.number*dt-0.0001;
+
+	if(this.vX>=0.0 && this.vY>=0.0)
+		frictionSign=1.0;
+	else if(this.vX<0.0 && this.vY<0.0)
+		frictionSign=-1.0;
+	if(this.frictionX>this.aX)
+		this.frictionX=this.aX;
+	if(this.frictionY>this.aY)
+		this.frictionY=this.aY;
+	  this.vX+=dt*(this.aX-frictionSign*this.frictionX);
+	  this.vY+=dt*(this.aY-frictionSign*this.frictionY);
+	  this.pX=this.startpX+((this.aX-frictionSign*this.frictionX)*this.time*this.time)/2.0;
+	  //this.vX;
+	  this.pY=this.startpY+((this.aY-frictionSign*this.frictionY)*this.time*this.time)/2.0;
+	  //this.vY;
+  }
+function drawRectangle(ctx,angle)
+{
+
+		ctx.moveTo(this.pX,this.pY);
+
+		var nextX=this.pX+Math.cos(angle*Math.PI/180.0)*this.width;
+		var nextY=this.pY+Math.sin(angle*Math.PI/180.0)*this.width;
+		ctx.lineTo(nextX, nextY);
+		nextX=nextX+Math.sin(angle*Math.PI/180.0)*this.height;
+		nextY=nextY-Math.cos(angle*Math.PI/180.0)*this.height;
+		ctx.lineTo(nextX, nextY);
+		nextX=nextX-Math.cos(angle*Math.PI/180.0)*this.width;
+		nextY=nextY-Math.sin(angle*Math.PI/180.0)*this.width;
+		ctx.lineTo(nextX, nextY);					
+		ctx.lineTo(this.pX, this.pY);
+		ctx.fill();
+
+}
+
+
+function valid()
+{				
+	if ((document.form1.length.value=="")||(document.form1.angle.value==""))
+	{
+		alert ("Any field can't be empty");
+		return false;
+	}
+
+	
+	else if ((parseFloat(document.form1.length.value)<50)||(parseFloat(document.form1.length.value)>2000))
+	{
+		alert ("length value should be in 50:2000 ");
+		return false;
+	}
+	else if ((parseFloat(document.form1.angle.value)<0)||(parseFloat(document.form1.angle.value)>90))
+	{
+		alert ("angle value should be in 0:90 ");
+		return false;
+	}
+	else 
+	{
+		//alert ("Zapisano pomyĹ›lnie, dziÄ™kujemy :)");
+		drawPlane();
+		return true;
+	}
+//draw();
+}
+var topX=0;
+var topY=50;
+function drawPlane()      
+{
+	const canvasElem = document.getElementById('canv');
+	const ctx = canvasElem.getContext('2d');
+	ctx.clearRect(0, 0, canvasElem.width, canvasElem.height);
+
+	var strtmp = document.form1.length.value;
+	var length=parseFloat(strtmp);
+	strtmp = document.form1.angle.value;
+	var angle=parseFloat(strtmp);
+	strtmp = document.form1.friction.value;
+	var friction=parseFloat(strtmp);
+	strtmp = document.form1.dt.value;
+	var dt=parseFloat(strtmp);
+	/*
+	var topX=0;
+	var topY=50;
+	*/
+	var sina=Math.sin(angle*Math.PI/180.0);
+	var cosa=Math.cos(angle*Math.PI/180.0) ;
+	g=9.81;
+	var rectLength=30;
+	var rectHeight=20;
+	var vX=0.0;
+	var vY=0.0;
+
+	var bottomX=topX+Math.cos(angle*Math.PI/180.0)*length;
+	var bottomY=topY+Math.sin(angle*Math.PI/180.0)*length;
+
+	var rectangle = new Rectangle(rectHeight,rectLength,topX,topY,vX,vY,g*sina*cosa,g*sina*sina,g*cosa*friction*cosa,g*cosa*friction*sina);
+	
+
+	var i=0;
+	//for(i=0; i<150;i++)
+	//while(rectangle.pX <bottomX- Math.cos(angle*Math.PI/180.0)*rectLength  && rectangle.pY < bottomY- Math.sin(angle*Math.PI/180.0)*rectLength)
+	//var interval=setInterval(function() {nextTime(bottomX,bottomY,angle,length,rectangle,ctx,canvasElem);if(++i>100) clearInterval(interval);}, 50);
+
+	var interval=setInterval(function() 
+	{
+		nextTime(bottomX,bottomY,angle,length,rectangle,ctx,canvasElem,dt);
+		if(rectangle.pX >=bottomX- Math.cos(angle*Math.PI/180.0)*rectLength  && rectangle.pY >= bottomY- Math.sin(angle*Math.PI/180.0)*rectLength)
+		{
+			clearInterval(interval);
+		}
+		
+	}
+			, dt*1000);
+
+	
+	/*	
+	var rectangleTopX=topX;wyczyscil
+	var rectangleTopY=topY;
+	
+
+	while(rectangleTopX <bottomX- Math.cos(angle*Math.PI/180.0)*rectLength  && rectangleTopY <bottomY- Math.sin(angle*Math.PI/180.0)*rectLength   )
+	{
+		ctx.clearRect(0, 0, canvasElem.width, canvasElem.height);
+
+		ctx.beginPath();
+		ctx.moveTo(topX,topY);
+		ctx.lineTo(bottomX,bottomY);
+		ctx.stroke();
+
+		rectangleTopX+=Math.cos(angle*Math.PI/180.0);
+		rectangleTopY+=Math.sin(angle*Math.PI/180.0);
+		
+		ctx.moveTo(rectangleTopX,rectangleTopY);
+		
+		var nextX=rectangleTopX+Math.cos(angle*Math.PI/180.0)*rectLength;
+		var nextY=rectangleTopY+Math.sin(angle*Math.PI/180.0)*rectLength;
+		ctx.lineTo(nextX, nextY);
+		nextX=nextX+Math.sin(angle*Math.PI/180.0)*rectHeight;
+		nextY=nextY-Math.cos(angle*Math.PI/180.0)*rectHeight;
+		ctx.lineTo(nextX, nextY);
+		nextX=nextX-Math.cos(angle*Math.PI/180.0)*rectLength;
+		nextY=nextY-Math.sin(angle*Math.PI/180.0)*rectLength;
+
+		ctx.lineTo(nextX, nextY);
+		ctx.lineTo(rectangleTopX, rectangleTopY);
+		ctx.fill();
+
+	}
+			
+*/
+
+	
+}
+function nextTime(bottomX,bottomY,angle,length,rectangle,ctx,canvasElem,dt)
+{
+	document.getElementById('timeDiv').innerHTML="czas: "+rectangle.time;
+	ctx.clearRect(0, 0, canvasElem.width, canvasElem.height);
+
+	ctx.beginPath();
+	ctx.moveTo(topX,topY);
+	ctx.lineTo(bottomX,bottomY);
+	ctx.stroke();
+	rectangle.drawRectangle(ctx,angle);
+	rectangle.nextPosition(dt);
+	
+	
+	
+	//alert(rectangle.pX+" "+ rectangle.pY);
+
+
+}
+
